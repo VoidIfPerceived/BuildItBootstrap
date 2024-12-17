919d8af957ad5257f122785bb90aaa3f24d0ea7c
VoidIfPerceived <sophi@offnominal.com>
2024-12-17 03:00:29 -0600

    added brand to navbars

--- src/Components/PageNavbar.js
+++ src/Components/PageNavbar.js
@@ -5,7 +5,6 @@ import Container from "react-bootstrap/Container";
 
 
 
-
 class PageNavbar extends React.Component {
     constructor(props) {
         super(props);
@@ -16,20 +15,27 @@ class PageNavbar extends React.Component {
         const NavElement = (page, index) => {
             return (
                 <Nav.Item key={index}>
-                    <Nav.Link href="page">{page.pageName ? page.pageName.toUpperCase() : "Page not found"}</Nav.Link>
+                    <h3><Nav.Link href={'#'+{page}}>{page.pageName ? page.pageName.toUpperCase() : "No pages found"}</Nav.Link></h3>
                 </Nav.Item>
             );
         };
 
 
         return (
-            <div>
+            <Container>
                 <Navbar sticky="top">
+                <Navbar.Brand href={"#home"}>
+                    <img
+                        src={this.props.brand}
+                        width="80"
+                        height="80"
+                    />
+                </Navbar.Brand >
                     <Container >
                         {this.props.pages && this.props.pages.map((page, index) => NavElement(page, index))}
                     </Container>
                 </Navbar>
-            </div>
+            </Container>
         )
 
     }
@@ -38,3 +44,7 @@ class PageNavbar extends React.Component {
 }
 
 export default PageNavbar
+
+//https://github.com/VoidIfPerceived/PromineoFinalProject/blob/935bc011eea437a9e93a170a4f4ac2e9900e0ecc/src/Images/BuildItBootstrap.png
+
+//./public/Images/BuildItBootstrap

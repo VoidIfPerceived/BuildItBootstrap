213366e8c9a2f8090fc9572c70a185ef9667b829
VoidIfPerceived <sophi@offnominal.com>
2024-12-21 21:55:42 -0600

    in process of updating react-router-dom to v7 (currently broken on v6)

--- src/Components/PageRouter.js
+++ src/Components/PageRouter.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {
     BrowserRouter as Router,
-    Switch,
+    Routes,
     Route,
     Link,
     useRouteMatch
@@ -18,21 +18,23 @@ export default class PageRouter extends React.Component {
 
     newRoute = (page, index) => {
         let path = "";
-        page.pageSlug == "home" ? path = "*" : path = "/" + page.pageSlug;
+        page.pageSlug === "home" ? path="/" : path=`/${page.pageSlug}`;
         console.log(page)
         return ( 
-            <Route key={index} path={path}>
-                <UserPage currentPage={page} onUpdate={this.props.onUpdate}/>
-            </Route>
+            <Route 
+                key={index} 
+                path={path} 
+                element={<UserPage currentPage={page} onUpdate={this.props.onUpdate}/>}
+            />
         )
     }
 
     render() {
         return (
-            <Router basename="home">
-                <Switch>
+            <Router>
+                <Routes>
                     {this.pages && this.pages.reverse().map((page, index) => this.newRoute(page, index))}
-                </Switch>
+                </Routes>
             </Router>
         )
     }
